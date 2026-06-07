668bd32b2cce90be8b4d388a9a8af5df1a670b51
Dishant Kumar <disha@example.com>
2026-06-07 09:07:15 +0530

    DAY - 1 uploaded

---
+++ DAY - 8/print_character_triangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n ;
+    char ch ;
+    printf("enter number of rows:");
+    scanf("%d",&n);
+    for(int i = 1 ; i<=n ; i++){ 
+        ch ='A' ;
+        for(int j = 1 ; j<=i ; j++){
+            printf("%c",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0 ;
+}
+    
